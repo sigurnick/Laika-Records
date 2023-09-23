c0f1fd6e9764e8f8588ab3dd48d6b43fd89c6247
sigurnick <nicolas94.m@gmail.com>
2023-09-23 17:40:15 +0200

    inserite tutte e 3 le sezioni

--- src/app/pages/about/about/about.component.ts
+++ src/app/pages/about/about/about.component.ts
@@ -9,18 +9,62 @@ import { Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild } fro
 })
 export class AboutComponent {
 
+  @ViewChild('section2') section2Element!: ElementRef;
 
+  private observer!: IntersectionObserver;
   isOnSection2:boolean = false;
-
+  private isScrollingEnabled = true;
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
 
-scrollToSection2() {
 
+  // ngOnInit() {
+  //   // Aggiungi l'event listener quando il componente è inizializzato
+  //   window.addEventListener('wheel', this.handleWheelEvent, { passive: false });
+  // }
+
+  // ngOnDestroy() {
+  //   // Rimuovi l'event listener quando il componente viene distrutto
+  //   window.removeEventListener('wheel', this.handleWheelEvent);
+  // }
+
+  // handleWheelEvent(event: WheelEvent) {
+  //   event.preventDefault();
+  //   // Implementa la logica di scorrimento personalizzato qui
+  // }
+
+
+
+
+
+
+
+  // @HostListener('window:keydown', ['$event'])
+  // handleKeyboardEvent(event: KeyboardEvent) {
+  //   // Disabilita lo scroll con le frecce su e giù sulla tastiera
+  //   if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+  //     event.preventDefault();
+  //   }
+  // }
+
+
+scrollToSection2() {
   const section2 = this.elementRef.nativeElement.querySelector('#section2'); //riferimento section2
   section2.scrollIntoView({ behavior: 'smooth' }); //scroll to section
-  this.isOnSection2 = true;
+
+}
+
+scrollToSection3() {
+  const section3 = this.elementRef.nativeElement.querySelector('#section3'); //riferimento section3
+  section3.scrollIntoView({ behavior: 'smooth' }); //scroll to section
+
+}
+
+
+scrollToSection1() {
+  const section1 = this.elementRef.nativeElement.querySelector('#section1'); //riferimento section2
+  section1.scrollIntoView({ behavior: 'smooth' }); //scroll to section
 
 }
 
