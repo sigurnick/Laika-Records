b5472a090a966ac38e696e33cc8bbf6578ff26c8
sigurnick <nicolas94.m@gmail.com>
2023-09-23 13:21:13 +0200

    create sezioni e animazione section2

--- src/app/pages/about/about/about.component.ts
+++ src/app/pages/about/about/about.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-about',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
+
+
+  isOnSection2:boolean = false;
+
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
+
+
+scrollToSection2() {
+
+  const section2 = this.elementRef.nativeElement.querySelector('#section2'); //riferimento section2
+  section2.scrollIntoView({ behavior: 'smooth' }); //scroll to section
+  this.isOnSection2 = true;
+
+}
 
 }
